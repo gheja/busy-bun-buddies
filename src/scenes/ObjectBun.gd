3e337d85de910b3889564e7c316ecbdbba54b911
Gabor Heja <gheja@uw830g8.git.stdio.hu>
2023-08-07 06:28:45 +0000

    2023-08-05 23:58

--- src/scenes/ObjectBun.gd
+++ src/scenes/ObjectBun.gd
@@ -2,7 +2,6 @@ extends KinematicBody2D
 
 onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 
-var animation_base_name = "idle"
 var current_direction_name = "down"
 
 var direction = Vector2.DOWN
@@ -26,7 +25,15 @@ var farmer_crops_held = 0
 var target_reached = false
 var target_object: Node2D = null
 var secondary_object: Node2D = null
+var task_steps_left = 0
+var task_animation_name = "idle"
 
+# [ [ time needed to finish, animation name ], ... ]
+var task_definitions = [
+	[ 0, "idle" ],
+	[ 3, "working" ],
+	[ 3, "working" ],
+]
 
 # --- needs ---
 var tiredness = 0
@@ -38,13 +45,15 @@ func _ready():
 	nav_agent.max_speed = 100
 	nav_agent.radius = 15
 	nav_agent.avoidance_enabled = true
+	
+	update_carry_container()
 
 func update_direction_name():
-	if velocity.y < 0.1:
+	if direction.y < 0.1:
 		current_direction_name = "up"
-	elif velocity.x < 0.1:
+	elif direction.x < 0.1:
 		current_direction_name = "left"
-	elif velocity.x > 0.1:
+	elif direction.x > 0.1:
 		current_direction_name = "right"
 	else:
 		current_direction_name = "down"
@@ -53,12 +62,16 @@ func update_velocity():
 	velocity = direction * speed
 
 func update_animation():
+	var animation_base_name = "idle"
 	var a
 	
-	if abs(velocity.x) > 0.1 or abs(velocity.y) > 0.1:
-		animation_base_name = "walk"
+	if target_reached and task_steps_left > 0:
+		animation_base_name = task_animation_name
 	else:
-		animation_base_name = "idle"
+		if abs(velocity.x) > 0.1 or abs(velocity.y) > 0.1:
+			animation_base_name = "walk"
+		else:
+			animation_base_name = "idle"
 	
 	if animation_base_name == "idle":
 		a = "idle"
@@ -98,6 +111,12 @@ func _physics_process(_delta):
 
 # --- job and task related functions ---
 
+func update_carry_container():
+	$CarryContainer/Tomato.hide()
+	
+	if farmer_crops_held > 0:
+		$CarryContainer/Tomato.show()
+
 func arrived_to_target():
 	print("arrived_to_target()")
 	
@@ -118,6 +137,8 @@ func set_task(new_task: int, obj: Node2D = null, claim: bool = false, secondary_
 	target_object = obj
 	secondary_object = secondary_obj
 	target_reached = false
+	task_steps_left = task_definitions[task][0]
+	task_animation_name = task_definitions[task][1]
 	
 	if claim:
 		Lib.claim_object(target_object)
@@ -131,7 +152,19 @@ func swap_task_objects():
 	target_object = secondary_object
 	secondary_object = tmp
 
+# decreases the steps left in the task and returns true if finished,
+# false otherwise
+func do_task_and_is_finished():
+	if task_steps_left > 0:
+		task_steps_left -= 1
+	
+	if task_steps_left == 0:
+		return true
+	
+	return false
+
 func think():
+	print("")
 	print("think()")
 	
 	var obj
@@ -151,21 +184,24 @@ func think():
 				# obj = Lib.get_nearest_object(self, "plant", true)
 		elif task == TASK_COLLECTING:
 			if target_reached:
-				print("collecting")
-				target_object.crop()
-				farmer_crops_held += 1
-				set_task(TASK_IDLING, null, false)
+				print("collecting...")
+				if do_task_and_is_finished():
+					target_object.crop()
+					farmer_crops_held += 1
+					set_task(TASK_IDLING, null, false)
 		elif task == TASK_DROPPING_OFF:
 			if target_reached:
-				print("dropping off")
-				target_object.crops_in(farmer_crops_held)
-				farmer_crops_held = 0
-				set_task(TASK_IDLING, null, false)
+				print("dropping off...")
+				if do_task_and_is_finished():
+					target_object.crops_in(farmer_crops_held)
+					farmer_crops_held = 0
+					set_task(TASK_IDLING, null, false)
 	
-	if Lib.is_object_valid(target_object):
+	if Lib.is_object_valid(target_object) and not target_reached:
 		nav_agent.set_target_location(target_object.global_position)
 		is_navigating = true
 	
+	update_carry_container()
 
 func _on_Timer_timeout():
 	think()
