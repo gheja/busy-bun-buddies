a7a16840adbee6c8214454d1bfa8accc779d2ef0
Gabor Heja <gheja@uw830g8.git.stdio.hu>
2023-08-13 21:59:19 +0000

    Added title screen and intro, now it loads by default

---
+++ src/scenes/TitleScreen.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var intro_finished = false
+
+func set_intro_finished():
+	intro_finished = true
+
+func _unhandled_input(event):
+	if not intro_finished:
+		return
+	
+	if (event is InputEventMouseButton) or (event is InputEventKey):
+		get_tree().change_scene("res://scenes/MainScene.tscn")
